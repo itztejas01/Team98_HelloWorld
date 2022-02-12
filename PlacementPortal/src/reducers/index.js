c58cfedfbe8ba9ae12668e058e6bb901a5d33119
Kashyap Kotak <kotakkashyap2803@gmail.com>
2022-02-12 20:49:22 +0530

    dashboard for super admin made

--- PlacementPortal/src/reducers/index.js
+++ PlacementPortal/src/reducers/index.js
@@ -1,6 +1,8 @@
 import {combineReducers} from 'redux';
 import LoginReducer from './LoginReducer';
+import SuperAdminReducer from './SuperAdminReducer';
 
 export default combineReducers({
     login: LoginReducer,
+    superAdmin: SuperAdminReducer,
 })
--- PlacementPortal/src/routes/MainAppRoutes.js
+++ PlacementPortal/src/routes/MainAppRoutes.js
@@ -4,13 +4,14 @@ import React from "react";
 import HomeScreen from "../components/HomeScreen";
 import Login from "../components/Login";
 import SplashScreen from "../components/SplashScreen";
+import SuperAdminDashboard from "../components/superadminScreens/SuperAdminDashboard";
 
 const MainStack = createNativeStackNavigator();
 
 function MainAppRoutes (){
     return( <NavigationContainer>
-        <MainStack.Navigator initialRouteName="Login">
-            {/* <MainStack.Screen name='Splash' component={SplashScreen} /> */}
+        <MainStack.Navigator initialRouteName="SuperAdminDash">
+            <MainStack.Screen name='SuperAdminDash' component={SuperAdminDashboard} />
             <MainStack.Screen name='Login' component={Login} />
             <MainStack.Screen name='HomeScreen' component={HomeScreen} />
         </MainStack.Navigator>
