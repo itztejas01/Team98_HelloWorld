8f99476501b8ca96e4798fe2d5ce50ecb45b26ca
Prince Karania <princekarania@gmail.com>
2022-02-12 22:56:33 +0530

    action type

--- PlacementPortal/src/actions/actionTypes.js
+++ PlacementPortal/src/actions/actionTypes.js
@@ -1,3 +1,2 @@
 export const USERNAME_TEXT_FIELD_CHANGED = "username_textfield_changed";
 export const PASSWORD_TEXTFIELD_CHANGED = "password_textfield_changed";
-export const TOGGLE_LOGIN_LOADER = "toggle_login_loader";
