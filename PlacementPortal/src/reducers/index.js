3198916fa966b75535610876ff2e35b44389d76d
Kashyap Kotak <kotakkashyap2803@gmail.com>
2022-02-13 11:52:18 +0530

    prn number redux done

--- PlacementPortal/src/reducers/index.js
+++ PlacementPortal/src/reducers/index.js
@@ -1,8 +1,10 @@
 import {combineReducers} from 'redux';
 import LoginReducer from './LoginReducer';
+import StudentReducer from './StudentReducer';
 import SuperAdminReducer from './SuperAdminReducer';
 
 export default combineReducers({
     login: LoginReducer,
     superAdmin: SuperAdminReducer,
+    student: StudentReducer,
 })
